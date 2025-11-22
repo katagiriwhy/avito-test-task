ee0d5d2f9f2d76fd34097b02692d7256ac43eef4
katagiri <karuk.vova2005@mail.ru>
2025-11-22 17:51:34 +0300

    feature: implemented user_service

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -2,23 +2,50 @@ package services
 
 import (
 	"context"
+	"errors"
 
+	"github.com/jackc/pgx/v5"
+	appError "github.com/katagiriwhy/avito-test-task/internal/errors"
 	"github.com/katagiriwhy/avito-test-task/internal/models"
 	"github.com/katagiriwhy/avito-test-task/internal/storage"
 )
 
 type UserService struct {
-	db *storage.PostgresStorage
+	storage storage.Storage
 }
 
-func NewUserService(db *storage.PostgresStorage) *UserService {
-	return &UserService{db: db}
+func NewUserService(storage storage.Storage) *UserService {
+	return &UserService{storage: storage}
 }
 
-func (u *UserService) SetIsActive(ctx context.Context, userId string, isActive bool) error {
-	return u.db.SetIsActive(ctx, userId, isActive)
+func (u *UserService) SetIsActive(ctx context.Context, userId string, isActive bool) (*models.User, error) {
+	updated, err := u.storage.SetIsActive(ctx, userId, isActive)
+	if err != nil {
+		return nil, err
+	}
+	if !updated {
+		return nil, appError.NewNotFound("NOT_FOUND", "user not found")
+	}
+
+	user, err := u.storage.GetUser(ctx, userId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, appError.NewNotFound("NOT_FOUND", "user not found")
+		}
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (u *UserService) GetReviews(ctx context.Context, userId string) ([]models.PullRequestShort, error) {
-	return u.db.GetReview(ctx, userId)
+	_, err := u.storage.GetUser(ctx, userId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, appError.NewNotFound("NOT_FOUND", "user not found")
+		}
+		return nil, err
+	}
+
+	return u.storage.GetReview(ctx, userId)
 }
